94ded7f3c758cf5e260e6ac6d5c4c95ed17877a1
hityzy <18B303002@stu.hit.edu.cn>
2018-07-05 14:19:36 +0800

    histogram normalization

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ PATH_TFRECORDS_TRAIN = './data/tfrecords/train.tfrecords'
 PATH_TFRECORDS_VALIDATION = './data/tfrecords/val.tfrecords'
 PATH_TRAIN_LIST = './data/sample_train.txt'
 PATH_VAL_LIST = './data/sample_val.txt'
+PATH_TEST_IMAGES = './data/test/'
 
 # PATH_IMAGES = 'C:\\workshop\\IDcard_OCR\\text_generator\\output\\textes\\Train'
 # PATH_TFRECORDS_VALIDATION = 'C:\\workshop\\IDcard_OCR\\text_generator\\output\\textes\\val.tfrecords'
@@ -24,3 +25,5 @@ TRAIN_SEQUENCE_LENGTH = 25
 TRAIN_EPHO = 100000
 TRAIN_DISPLAY = 1
 TRAIN_VAL_SNAPSHOT = 1000
+
+TEST_CHECKPOINTS = './checkpoints/20180625-2107/'
