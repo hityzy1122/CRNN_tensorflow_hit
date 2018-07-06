43efa6ac06a2f0a7883056e9d25da8f4bd42675c
hityzy <18B303002@stu.hit.edu.cn>
2018-07-06 16:26:43 +0800

    modified

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ PATH_DICT_TXT = './data/chinese_dict.txt'
 PATH_STR_2_LABEL_DICT = './data/dict/str_2_label.json'
 PATH_LABEL_2_STR_DICT = './data/dict/label_2_str.json'
 PATH_CHECKPOINTS = './checkpoints'
-
 PATH_IMAGES = 'data/images'
 PATH_TFRECORDS_TRAIN = './data/tfrecords/train.tfrecords'
 PATH_TFRECORDS_VALIDATION = './data/tfrecords/val.tfrecords'
@@ -13,8 +12,8 @@ PATH_TRAIN_LIST = './data/sample_train.txt'
 PATH_VAL_LIST = './data/sample_val.txt'
 PATH_TEST_IMAGES = './data/test/'
 
-# PATH_IMAGES = 'C:\\workshop\\IDcard_OCR\\text_generator\\output\\textes\\Train'
-# PATH_TFRECORDS_VALIDATION = 'C:\\workshop\\IDcard_OCR\\text_generator\\output\\textes\\val.tfrecords'
+IMAGE_WIDTH = 100
+IMAGE_HEIGHT = 32
 
 TRAIN_BATCH_SIZE = 128
 TRAIN_VAL_BATCH_SIZE = 128
